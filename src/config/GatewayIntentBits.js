d8e1d51889cc9bfce9b7627089a02ca39b694661
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-08 12:19:41 -0800

    d

--- src/config/GatewayIntentBits.js
+++ src/config/GatewayIntentBits.js
@@ -2,3 +2,4 @@ const { GatewayIntentBits } = require('discord.js');
 
 //exports.GatewayIntentBits = void 0;
 module.exports.GatewayIntentBits = [GatewayIntentBits.Guilds];
+console.log(GatewayIntentBits);
--- src/index.js
+++ src/index.js
@@ -71,7 +71,7 @@ async function getXRPToken() {
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.on(Events.ClientReady, c => {
-    console.log(GatewayIntentsBits.GatewayIntentsBits);
+    console.log(GatewayIntentsBits);
 	console.log(`Ready! Logged in as ${c.user.tag}`);
     const command = [ping, beep, xrplToken];
     //console.log(command);
