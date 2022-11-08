a5d2d2e7143ab716313d60294a127449a438722e
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-08 12:00:59 -0800

    d

--- src/index.js
+++ src/index.js
@@ -70,6 +70,7 @@ async function getXRPToken() {
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.on(Events.ClientReady, c => {
+    console.log(GatewayIntentsBits);
 	console.log(`Ready! Logged in as ${c.user.tag}`);
     const command = [ping, beep, xrplToken];
     //console.log(command);
