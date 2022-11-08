3dd1840665597bc52018801edd297e63e045f740
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-08 12:18:12 -0800

    d

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const { Client, Events, GatewayIntentsBits, SlashCommandBuilder } = require('discord.js');
+const { Client, Events, SlashCommandBuilder } = require('discord.js');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
 const { token, clientId, guildId } = require('../config.json');
 const axios = require('axios');
 const Database = require('better-sqlite3');
-const GatewayIntentBits = require('./config/GatewayIntentBits.js');
+const { GatewayIntentBits } = require('./config/GatewayIntentBits.js');
 
 // Create a new client instance
 //const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-//const client = new Client({ intents: GatewayIntentBits });
-const client = new Client({ intents: GatewayIntentBits.Guilds });
+const client = new Client({ intents: GatewayIntentBits });
+//const client = new Client({ intents: GatewayIntentBits.Guilds });
 
 const db = new Database('./data/tokens.db');
 
