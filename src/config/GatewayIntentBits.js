b0770d0743680ad7f0e312885cb5d0e62edbea06
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-08 19:09:08 -0800

    d

--- src/config/GatewayIntentBits.js
+++ src/config/GatewayIntentBits.js
@@ -1,5 +1,4 @@
 const { GatewayIntentBits } = require('discord.js');
 
 //exports.GatewayIntentBits = void 0;
-module.exports.GatewayIntentBits = [GatewayIntentBits.Guilds];
-console.log(GatewayIntentBits);
+module.exports.GatewayIntentBits = ["GatewayIntentBits.Guilds"];
