dbb2e94ff3424e42c3a01a21872b4f8b27e3c513
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-10 14:40:49 -0800

    d

--- events/onReady.js
+++ events/onReady.js
@@ -1,7 +1,7 @@
 const client = require("..");
 const { Events } = require('discord.js');
 
-export const onReady = async (client) => {
+const onReady = async (client) => {
     client.on(Events.ClientReady, client => {
         console.log(`Ready! Logged in as ${client.user.tag}`);
     });
