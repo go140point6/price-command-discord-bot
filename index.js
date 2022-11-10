6b3b6fddff56411d6838c9c654f9a10a44fba58b
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-10 14:27:10 -0800

    d

--- index.js
+++ index.js
@@ -1,37 +1,17 @@
 require('dotenv').config();
 require('log-timestamp');
-// Node's native file system module. fs is used to read the commands directory and identify our command files.
-const fs = require('node:fs');
-// Node's native path utility module. path helps construct paths to access files and directories. One of the advantages of the path module is that it automatically detects the operating system and uses the appropriate joiners.
-const path = require('node:path');
-const { Client, Collection } = require('discord.js');
+const { Client } = require('discord.js');
 const { GatewayIntentBits } = require('./config/GatewayIntentBits');
 const { validateEnv } = require('./utils/validateEnv');
-const { onInteraction } = require('./events/onInteraction');
 
 (async () => {
     validateEnv();
 
     const client = new Client({ intents: GatewayIntentBits });
 
-    // extends JavaScript's native Map class, and includes more extensive, useful functionality. Collection is used to store and efficiently retrieve commands for execution.
-    client.commands = new Collection();
-
-    const commandsPath = path.join(__dirname, 'commands');
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-    for (const file of commandFiles) {
-	    const filePath = path.join(commandsPath, file);
-	    const command = require(filePath);
-	    // Set a new item in the Collection with the key as the command name and the value as the exported module
-	    if ('data' in command && 'execute' in command) {
-    		client.commands.set(command.data.name, command);
-	    } else {
-		    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-    	}
-    }
-
-    module.exports = client;
-
+    Client.on(Events.ClientReady, client => {
+        console.log(`Ready! Logged in as ${client.user.tag}`);
+    });
+    
     await client.login(process.env.BOT_TOKEN);
 })();
