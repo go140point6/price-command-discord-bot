69a01fddf890bf8cc505f830990d26666161e3ec
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-10 15:20:38 -0800

    d

--- events/onReady.js
+++ events/onReady.js
@@ -1,10 +1,10 @@
 const { Client, Events } = require('discord.js');
 const client = require('../index');
 
-module.exports = {
-    name: "onReady.js"
-};
-
 function onReady(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`)
 };
+
+module.exports = { 
+    onReady 
+}
