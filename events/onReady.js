e13af67047de17e440b38536a9c9ca49f1371bf3
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-10 15:58:12 -0800

    d

--- events/onReady.js
+++ events/onReady.js
@@ -9,6 +9,22 @@ const { REST, Routes } = require('discord.js');
 function onReady(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`)
 
+    client.commands = new Collection();
+
+    const commandsPath = path.join(__dirname, 'commands');
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+
     const commands = [];
     // Grab all the command files from the commands directory 
     const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
--- index.js
+++ index.js
@@ -15,22 +15,6 @@ const { validateEnv } = require('./utils/validateEnv');
     const client = new Client({ intents: GatewayIntentBits });
     module.exports = client;
 
-    client.commands = new Collection();
-
-    const commandsPath = path.join(__dirname, 'commands');
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        // Set a new item in the Collection with the key as the command name and the value as the exported module
-        if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command);
-        } else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-        }
-    }
-
     client.on(Events.ClientReady, async() => await onReady(client));
     
     await client.login(process.env.BOT_TOKEN);
