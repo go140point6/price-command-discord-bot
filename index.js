e7da2a180788c40216ea4134f16eeaebf7718a98
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-10 15:56:55 -0800

    d

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
 require('log-timestamp');
+// Node's native file system module. fs is used to read the commands directory and identify our command files.
+const fs = require('node:fs');
+// Node's native path utility module. path helps construct paths to access files and directories. One of the advantages of the path module is that it automatically detects the operating system and uses the appropriate joiners.
+const path = require('node:path');
 const { Client, Collection, Events } = require('discord.js');
 const { GatewayIntentBits } = require('./config/GatewayIntentBits');
 const { onReady } = require('./events/onReady');
@@ -12,6 +16,20 @@ const { validateEnv } = require('./utils/validateEnv');
     module.exports = client;
 
     client.commands = new Collection();
+
+    const commandsPath = path.join(__dirname, 'commands');
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
 
     client.on(Events.ClientReady, async() => await onReady(client));
     
