205975e8b71e5fbc3c99f29fda32db9de3a5816a
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-10 15:23:39 -0800

    d

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const { Client, Events } = require('discord.js');
 const { GatewayIntentBits } = require('./config/GatewayIntentBits');
 const { onReady } = require('./events/onReady');
 const { validateEnv } = require('./utils/validateEnv');
-//const { onReady } = require('./events/onReady');
 
 (async () => {
     validateEnv();
@@ -12,18 +11,7 @@ const { validateEnv } = require('./utils/validateEnv');
     const client = new Client({ intents: GatewayIntentBits });
     module.exports = client;
 
-    /*
-    client.on(Events.ClientReady, async() => {
-        console.log(`Ready! Logged in as ${client.user.tag}`)
-    });
-    */
-
     client.on(Events.ClientReady, async() => await onReady(client));
     
-    /*
-    await onReady(client));
-    module.exports = client;
-    */
-
     await client.login(process.env.BOT_TOKEN);
 })();
