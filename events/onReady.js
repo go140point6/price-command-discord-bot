01c760ae707d984bd9de5603ac7c71cf7290a00f
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-10 15:37:30 -0800

    d

--- events/onReady.js
+++ events/onReady.js
@@ -8,9 +8,40 @@ const { REST, Routes } = require('discord.js');
 
 function onReady(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`)
+
+    const commands = [];
+// Grab all the command files from the commands directory 
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
+
+// and deploy your commands!
+(async () => {
+	try {
+		// The put method is used to fully refresh all commands in the guild with the current set.
+		const data = await rest.put(
+			Routes.applicationGuildCommands(
+                process.env.CLIENT_ID, 
+                process.env.GUILD_ID
+                ),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// Catch and log any errors.
+		console.error(error);
+	}
+})();
 };
 
 module.exports = { 
     onReady 
 }
-
