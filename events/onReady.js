860426b98883db1cdc21dccd17e559f5cb103283
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-10 15:33:34 -0800

    d

--- events/onReady.js
+++ events/onReady.js
@@ -1,5 +1,10 @@
-//const { Client, Events } = require('discord.js');
+require('dotenv').config();
+// Node's native file system module. fs is used to read the commands directory and identify our command files.
+const fs = require('node:fs');
+// Node's native path utility module. path helps construct paths to access files and directories. One of the advantages of the path module is that it automatically detects the operating system and uses the appropriate joiners.
+const path = require('node:path');
 const client = require('../index');
+const { REST, Routes } = require('discord.js');
 
 function onReady(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`)
@@ -8,3 +13,4 @@ function onReady(client) {
 module.exports = { 
     onReady 
 }
+
