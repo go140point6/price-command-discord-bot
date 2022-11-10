e972eaed1c922dbd149fa9ebd4c458ef60beaab1
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-10 15:47:05 -0800

    d

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 require('log-timestamp');
-const { Client, Events } = require('discord.js');
+const { Client, Collection, Events } = require('discord.js');
 const { GatewayIntentBits } = require('./config/GatewayIntentBits');
 const { onReady } = require('./events/onReady');
 const { validateEnv } = require('./utils/validateEnv');
@@ -10,6 +10,8 @@ const { validateEnv } = require('./utils/validateEnv');
 
     const client = new Client({ intents: GatewayIntentBits });
     module.exports = client;
+
+    client.commands = new Collection();
 
     client.on(Events.ClientReady, async() => await onReady(client));
     
