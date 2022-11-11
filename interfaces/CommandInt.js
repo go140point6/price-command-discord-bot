6d0223192dc3db53c1cab361a383500d41d68ff4
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-11 08:33:38 -0800

    d

--- interfaces/CommandInt.js
+++ interfaces/CommandInt.js
@@ -9,7 +9,7 @@ const CommandInt = function () {
     client.commands = new Collection();
 
     //const commandsPath = path.join(__dirname, 'commands');
-    const commandsPath = path.basename('../commands', 'commands');
+    const commandsPath = path.join(__dirname, '..', 'commands');
     const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
     for (const file of commandFiles) {
