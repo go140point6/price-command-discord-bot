f4f1c1b8a69871483edad2c609132094d8577521
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-11 08:21:13 -0800

    d

--- commands/_CommandList.js
+++
@@ -1,6 +0,0 @@
-import { CommandInt } from "../interfaces/CommandInt";
-import { ping } from "./ping";
-import { beep } from "./beep";
-import { xrplToken } from "./xrplToken";
-
-exports.CommandList = [ping, beep, xrplToken];
