059a75cf2c3e51012b3f7f4982124fed4caecc77
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-10 16:12:46 -0800

    d

--- events/onReady.js
+++ events/onReady.js
@@ -5,25 +5,10 @@ const fs = require('node:fs');
 const path = require('node:path');
 const client = require('../index');
 const { REST, Routes } = require('discord.js');
+const { CommandInt } = require('../interfaces/CommandInt');
 
 function onReady(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`)
-
-    client.commands = new Collection();
-
-    const commandsPath = path.join(__dirname, 'commands');
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        // Set a new item in the Collection with the key as the command name and the value as the exported module
-        if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command);
-        } else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-        }
-    }
 
     const commands = [];
     // Grab all the command files from the commands directory 
--- index.js
+++ index.js
@@ -15,7 +15,26 @@ const { validateEnv } = require('./utils/validateEnv');
     const client = new Client({ intents: GatewayIntentBits });
     module.exports = client;
 
+    async function CommandInt();
+
     client.on(Events.ClientReady, async() => await onReady(client));
     
     await client.login(process.env.BOT_TOKEN);
 })();
+
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
