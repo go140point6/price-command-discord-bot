4c1c01f457f8b9f1faca474712688f6e5440e9a0
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-11 08:44:16 -0800

    d

--- interfaces/CommandInt.js
+++ interfaces/CommandInt.js
@@ -9,22 +9,24 @@ const CommandInt = function () {
     client.commands = new Collection();
 
     //const commandsPath = path.join(__dirname, 'commands');
-    const commandsPath = path.join(__dirname, '../commands');
+    const commandsPath = path.join(__dirname, '..', 'commands');
     console.log(commandsPath);
-    
-    /*
+
     const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    console.log(commandFiles);
 
     for (const file of commandFiles) {
         const filePath = path.join(commandsPath, file);
+        console.log(filePath);
         const command = require(filePath);
+        console.log(command);
         // Set a new item in the Collection with the key as the command name and the value as the exported module
         if ('data' in command && 'execute' in command) {
             client.commands.set(command.data.name, command);
         } else {
             console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
         }
-    }*/
+    }
 };
 
 module.exports = { 
