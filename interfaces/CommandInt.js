6ac76f65befe40bbc2fd1bdb2889abc5f0f55a64
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-10 16:20:32 -0800

    d

--- interfaces/CommandInt.js
+++ interfaces/CommandInt.js
@@ -1,3 +1,7 @@
+// Node's native file system module. fs is used to read the commands directory and identify our command files.
+const fs = require('node:fs');
+// Node's native path utility module. path helps construct paths to access files and directories. One of the advantages of the path module is that it automatically detects the operating system and uses the appropriate joiners.
+const path = require('node:path');
 const client = require('../index');
 const { Collection } = require('discord.js');
 
