29b2e4e752ae9eb08dcb92d3884f2b21585385f1
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-13 18:19:24 -0800

    d

--- events/onInteraction.js
+++ events/onInteraction.js
@@ -3,6 +3,6 @@ const { onInteraction } = require('discord.js');
 // Define an exported async function, with a parameter 'interaction'
 module.exports.onInteraction = async (interaction) => {
     if (interaction.isCommand()) {
-    
+        console.log("good job");
     }
 };
