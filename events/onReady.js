099a1427c392a4742469512127a792f4cf046663
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-14 12:56:47 -0800

    d

--- events/onReady.js
+++ events/onReady.js
@@ -60,9 +60,12 @@ function onReady(client) {
 async function getXRP() {
     await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ripple`).then(res => {
                if (res.data && res.data[0].current_price) {
-                const currentXRP = { price: res.data[0].current_price.toFixed(4) || 0 }
-                console.log("XRP current price inside: " + getXRP.currentXRP.price);
-                return getXRP.currentXRP;
+                const XRPprice = res.data[0].current_price.toFixed(4) || 0 
+                console.log("XRP current price inside: " + XRPprice);
+                let currentXRP = { price: XRPprice};
+                module.exports.setPrice = (price) => {
+                    currentXRP.price = price;
+                };
             } else {
                 console.log("Error loading coin data")
             }
