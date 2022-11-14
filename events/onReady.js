3a6484da18075f8a00a25136251feb5aa03ca3f8
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-13 19:23:33 -0800

    d

--- events/onReady.js
+++ events/onReady.js
@@ -61,7 +61,7 @@ async function getXRP() {
     await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ripple`).then(res => {
                if (res.data && res.data[0].current_price) {
                 currentXRP = res.data[0].current_price.toFixed(4) || 0
-                console.log("XRP current price: " + currentXRP);
+                //console.log("XRP current price: " + currentXRP);
             } else {
                 console.log("Error loading coin data")
             }
