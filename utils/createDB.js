5bae3a048746f3002481ecd85f3445d5f8e0e2da
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-15 12:07:22 -0800

    d

--- utils/createDB.js
+++ utils/createDB.js
@@ -6,7 +6,7 @@ const Database = require('better-sqlite3');
 const db = new Database('../data/tokens.db', {verbose: console.log });
 
 var tableName = "tokens";
-var fields = "(issuer TEXT PRIMARY KEY NOT NULL, currency TEXT, name TEXT, logo_file TEXT)";
+var fields = "(id INTEGER PRIMARY KEY AUTOINCREMENT, currency TEXT, issuer TEXT, name TEXT, logo_file TEXT)";
 var sql = `CREATE TABLE IF NOT EXISTS ${tableName} ${fields}`;
 const createTable = db.prepare(sql);
 createTable.run();
@@ -14,8 +14,8 @@ createTable.run();
 async function getTokens() {
     await axios.get(`https://api.onthedex.live/public/v1/aggregator`).then(res => {
         console.log(res.data.tokens);
-        const insert = db.prepare(`INSERT INTO tokens (issuer, currency, name, logo_file) VALUES (@issuer, @currency, @name, @logo_file)`);
-        //var insertQuery = "INSERT INTO tokens VALUES (?,?,?,?,?)";
+        let id = null;
+        const insert = db.prepare(`INSERT INTO tokens (id, currency, issuer, name, logo_file) VALUES (${id}, @currency, @issuer, @name, @logo_file)`);
 
         const insertMany = db.transaction((tokens) => {
             for (const token of tokens) insert.run(token)
