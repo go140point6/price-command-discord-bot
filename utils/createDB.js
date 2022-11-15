ddbbd25659f7b728e494de761f732c292c72310b
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-15 11:29:32 -0800

    d

--- utils/createDB.js
+++ utils/createDB.js
@@ -6,7 +6,7 @@ const Database = require('better-sqlite3');
 const db = new Database('../data/tokens.db', {verbose: console.log });
 
 var tableName = "tokens";
-var fields = "(id INTEGER PRIMARY KEY AUTOINCREMENT, currency TEXT, issuer TEXT)";
+var fields = "(issuer TEXT PRIMARY KEY NOT NULL, currency TEXT, name TEXT, logo_file TEXT)";
 var sql = `CREATE TABLE IF NOT EXISTS ${tableName} ${fields}`;
 const createTable = db.prepare(sql);
 createTable.run();
@@ -14,35 +14,22 @@ createTable.run();
 async function getTokens() {
     await axios.get(`https://api.onthedex.live/public/v1/aggregator`).then(res => {
         console.log(res.data.tokens);
-        let count = 0;
-        let id = null;
-        const insert = db.prepare(`INSERT INTO tokens (id, currency, issuer) VALUES (${id}, @currency, @issuer)`);
-        //var insertQuery = "INSERT INTO tokens VALUES (?,?,?)";
+        const insert = db.prepare(`INSERT INTO tokens (issuer, currency, name, logo_file) VALUES (@issuer, @currency, @name, @logo_file)`);
+        //var insertQuery = "INSERT INTO tokens VALUES (?,?,?,?,?)";
 
         const insertMany = db.transaction((tokens) => {
             for (const token of tokens) insert.run(token)
         })
 
         insertMany(res.data.tokens);
-        
-        //db.prepare(insertQuery).run(id, currency, issuer);
-        
-        //const stmt = db.prepare("SELECT * FROM tokens");
-        //var results = stmt.all();
-        //console.log(results);
-
     });
 };
 
 async function grabTokens() {
     await getTokens();
     const stmt = db.prepare("SELECT * FROM tokens");
-    //const stmt2 = db.prepare("SELECT EXISTS (SELECT name FROM sqlite_schema WHERE type='table' AND name='tokens')");
-    //var results = stmt2.get();
     var results = stmt.all();
     console.log(results);
-    //console.log(Object.values(results));
-    //const isZero = results.every(item => item === 0)
 }
 
 grabTokens();
