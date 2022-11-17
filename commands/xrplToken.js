db59c07b333ecc6fd014520a7ad81ddec8fe0cac
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-16 16:36:17 -0800

    d

--- commands/xrplToken.js
+++ commands/xrplToken.js
@@ -5,14 +5,6 @@ const Database = require('better-sqlite3');
 
 const db = new Database('./data/data.db');
 
-const embedToken = new EmbedBuilder()
-    //.setColor('RANDOM')
-    .setTitle(`Current price of ${ticker} in USD`)
-    .addFields(
-        { name: ticker, value: inUSD },
-    )
-    .setImage(logo_file)
-
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('xrpl-token')
@@ -33,12 +25,21 @@ module.exports = {
         console.log("Current XRP price is $" + XRP.currentXRP);
         console.log("Number in array for " + ticker + " is " + results5.length);
 
+        let currency = results5[0].currency;
+        let issuer = results5[0].issuer;
+        let name = results5[0].name;
+        let logo_file = results5[0].logo_file;
+
+        const embedToken = new EmbedBuilder()
+            //.setColor('RANDOM')
+            .setTitle(`Current price of ${ticker} in USD`)
+            .addFields(
+                { name: ticker, value: inUSD },
+            )
+            .setImage(logo_file)
+
         if (Array.isArray(results5) && results5.length == 1) {
             //console.log("Array exists and has exactly 1 item");
-            let currency = results5[0].currency;
-            let issuer = results5[0].issuer;
-            let name = results5[0].name;
-            let logo_file = results5[0].logo_file;
             await axios.get(`https://api.onthedex.live/public/v1/ticker/${currency}.${issuer}:XRP`).then(res => {
                 if(res.data && res.data.pairs[0].last) {
                     const inXRP = res.data.pairs[0].last;
