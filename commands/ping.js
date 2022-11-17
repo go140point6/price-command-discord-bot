ec7221917a5c79fde28ef72d021a61e1b345e00d
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-16 16:27:45 -0800

    d

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,18 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+const embedPing = new EmbedBuilder()
+	.setColor('RANDOM')
+	.setTitle('Ping!')
+	.addFields(
+		{ name: 'Ping', value: 'Pong!'},
+	);
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		//await interaction.reply('Pong!');
+		await interaction.reply({ embeds: [embedPing] });
 	},
 };
