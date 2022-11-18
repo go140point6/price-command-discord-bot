e1eb15afd9d1487205e3a9177ac51480512183fd
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-17 20:16:09 -0800

    d

--- commands/xrplToken.js
+++ commands/xrplToken.js
@@ -65,7 +65,7 @@ module.exports = {
                 num++;
             }
             //return embedFields;
-            console.log("embedFields: " + embedFields);
+            console.log("embedFields: " + embedFields.toJSON());
 
 
 
