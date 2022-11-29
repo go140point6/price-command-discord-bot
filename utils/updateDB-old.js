f3e6469a1fd11c9ae828c3d506814504ae8a834f
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-11-29 09:51:44 -0800

    d

---
+++ utils/updateDB-old.js
@@ -0,0 +1,47 @@
+// Stand-alone script to update the DB of tokens
+// node updateDB.js
+const axios = require('axios');
+const Database = require('better-sqlite3');
+
+const db = new Database('../data/data.db', {verbose: console.log });
+
+//var tableName = "tokens";
+//var fields = "(id INTEGER PRIMARY KEY AUTOINCREMENT, currency TEXT, issuer TEXT)";
+//var sql = `CREATE TABLE IF NOT EXISTS ${tableName} ${fields}`;
+//const createTable = db.prepare(sql);
+//createTable.run();
+
+async function updateTokens() {
+    await axios.get(`https://api.onthedex.live/public/v1/aggregator`).then(res => {
+        let id = null;
+        const insert = db.prepare(`
+        INSERT INTO tokens (id, currency, issuer) VALUES (${id}, @currency, @issuer)`);
+        //var insertQuery = "INSERT INTO tokens VALUES (?,?,?)";
+
+        const insertMany = db.transaction((tokens) => {
+            for (const token of tokens) insert.run(token)
+        })
+
+        insertMany(res.data.tokens);
+        
+        //db.prepare(insertQuery).run(id, currency, issuer);
+        
+        //const stmt = db.prepare("SELECT * FROM tokens");
+        //var results = stmt.all();
+        //console.log(results);
+
+    });
+};
+
+async function grabTokens() {
+    await updateTokens();
+    const stmt = db.prepare("SELECT * FROM tokens");
+    //const stmt2 = db.prepare("SELECT EXISTS (SELECT name FROM sqlite_schema WHERE type='table' AND name='tokens')");
+    //var results = stmt2.get();
+    var results = stmt.all();
+    console.log(results);
+    //console.log(Object.values(results));
+    //const isZero = results.every(item => item === 0)
+}
+
+grabTokens();
--- utils/updateDB.js
+++ utils/updateDB.js
@@ -1,47 +1,80 @@
-// Stand-alone script to update the DB of tokens
+// Stand-alone script to update the existing DB of tokens
 // node updateDB.js
 const axios = require('axios');
 const Database = require('better-sqlite3');
 
-const db = new Database('../data/tokens.db', {verbose: console.log });
+const db = new Database('../data/data.db', {verbose: console.log });
 
-//var tableName = "tokens";
-//var fields = "(id INTEGER PRIMARY KEY AUTOINCREMENT, currency TEXT, issuer TEXT)";
-//var sql = `CREATE TABLE IF NOT EXISTS ${tableName} ${fields}`;
-//const createTable = db.prepare(sql);
-//createTable.run();
+var tableXrpl = "xrplTokens";
+var tableCrypto = "crypto"
+//var fieldsXrpl = "(id INTEGER PRIMARY KEY AUTOINCREMENT, currency TEXT, issuer TEXT, name TEXT, logo_file TEXT)";
+//var fieldsCrypto = "(id TEXT PRIMARY KEY, symbol TEXT, name TEXT)";
+//var sqlXrpl = `CREATE TABLE IF NOT EXISTS ${tableXrpl} ${fieldsXrpl}`;
+//var sqlCrypto = `CREATE TABLE IF NOT EXISTS ${tableCrypto} ${fieldsCrypto}`;
+//const createTableXrpl = db.prepare(sqlXrpl);
+//const createTableCrypto = db.prepare(sqlCrypto);
+//createTableXrpl.run();
+//createTableCrypto.run();
 
 async function updateTokens() {
     await axios.get(`https://api.onthedex.live/public/v1/aggregator`).then(res => {
+        //console.log(res.data.tokens);
         let id = null;
-        const insert = db.prepare(`
-        INSERT INTO tokens (id, currency, issuer) VALUES (${id}, @currency, @issuer)`);
-        //var insertQuery = "INSERT INTO tokens VALUES (?,?,?)";
+        const insertXrpl = db.prepare(`
+            INSERT INTO xrplTokens (id, currency, issuer, name, logo_file) 
+            VALUES (${id}, @currency, @issuer, @name, @logo_file)
+            SELECT currency, issuer
+            WHERE NOT EXISTS (SELECT 1 FROM xrplTokens WHERE currency = @currency AND issuer = @issuer)
+            `);
 
-        const insertMany = db.transaction((tokens) => {
-            for (const token of tokens) insert.run(token)
-        })
+        const insertManyXrpl = db.transaction((tokens) => {
+            for (const token of tokens) {
+                // undefined == null is true (equality operator) so both are true
+                // undefined === null is false (strict equality operator)
+                if (token.name == null) {
+                    token.name = null;
+                }
+                if (token.logo_file == null) {
+                    token.logo_file = null;
+                }
+            insertXrpl.run(token)
+            //console.log(token.name);
+            //console.log(token.logo_file);
+            }
+            
+        });
 
-        insertMany(res.data.tokens);
-        
-        //db.prepare(insertQuery).run(id, currency, issuer);
-        
-        //const stmt = db.prepare("SELECT * FROM tokens");
-        //var results = stmt.all();
-        //console.log(results);
-
+        insertManyXrpl(res.data.tokens);
     });
 };
 
+/*
+async function getCrypto() {
+    await axios.get(`https://api.coingecko.com/api/v3/coins/list?include_platform=false`).then(res => {
+        //console.log(res.data);
+        const insertCrypto = db.prepare(`INSERT INTO crypto (id, symbol, name) VALUES (@id, @symbol, @name)`);
+
+        const insertManyCrypto = db.transaction((cryptos) => {
+            for (const crypto of cryptos) {
+                insertCrypto.run(crypto)
+            }
+            
+        });
+
+        insertManyCrypto(res.data);
+    });
+};
+*/
+
 async function grabTokens() {
     await updateTokens();
-    const stmt = db.prepare("SELECT * FROM tokens");
-    //const stmt2 = db.prepare("SELECT EXISTS (SELECT name FROM sqlite_schema WHERE type='table' AND name='tokens')");
-    //var results = stmt2.get();
+    //const stmt = db.prepare("SELECT * FROM xrplTokens");
     var results = stmt.all();
     console.log(results);
-    //console.log(Object.values(results));
-    //const isZero = results.every(item => item === 0)
+    //await getCrypto();
+    //const stmt2 = db.prepare("SELECT * FROM crypto");
+    //var results2 = stmt2.all();
+    //console.log(results2);
 }
 
 grabTokens();
